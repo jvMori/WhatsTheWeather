3a09feec31050b251f0ab13a59a32f543488d356
Mori <iwona.moryc92@gmail.com>
2019-02-08 14:25:22 +0100

    weather Network Data Source

--- app/src/main/java/com/example/jvmori/myweatherapp/architectureComponents/data/network/WeatherNetworkDataSource.java
+++ app/src/main/java/com/example/jvmori/myweatherapp/architectureComponents/data/network/WeatherNetworkDataSource.java
@@ -2,10 +2,9 @@ package com.example.jvmori.myweatherapp.architectureComponents.data.network;
 
 import com.example.jvmori.myweatherapp.architectureComponents.data.network.response.CurrentWeatherResponse;
 
-import androidx.lifecycle.LiveData;
+import io.reactivex.Observable;
 
 public interface WeatherNetworkDataSource
 {
-    LiveData<CurrentWeatherResponse> currentWeather = null;
-    void fetchWeather(String location, String lang);
+    Observable<CurrentWeatherResponse> fetchWeather(String location, String lang);
 }
--- app/src/main/java/com/example/jvmori/myweatherapp/architectureComponents/data/network/WeatherNetworkDataSourceImpl.java
+++ app/src/main/java/com/example/jvmori/myweatherapp/architectureComponents/data/network/WeatherNetworkDataSourceImpl.java
@@ -2,41 +2,17 @@ package com.example.jvmori.myweatherapp.architectureComponents.data.network;
 
 import com.example.jvmori.myweatherapp.architectureComponents.data.network.response.CurrentWeatherResponse;
 
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
-import io.reactivex.Observer;
-import io.reactivex.SingleObserver;
+
+import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 
 public class WeatherNetworkDataSourceImpl implements WeatherNetworkDataSource{
-    private MutableLiveData<CurrentWeatherResponse> _currentWeather =  new MutableLiveData<>();
 
-    public LiveData<CurrentWeatherResponse> currentWeather(){
-        return _currentWeather;
-    }
-
-    public void fetchWeather(String location, String lang){
+    public Observable<CurrentWeatherResponse> fetchWeather(String location, String lang){
         ApixuApi service = ApixuApiCall.init();
-        service.getCurrentWeather(location, lang)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new SingleObserver<CurrentWeatherResponse>() {
-                    @Override
-                    public void onSubscribe(Disposable d) {
-
-                    }
-
-                    @Override
-                    public void onSuccess(CurrentWeatherResponse currentWeatherResponse) {
-                        _currentWeather.postValue(currentWeatherResponse);
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-
-                    }
-                });
+        return service.getCurrentWeather(location, lang)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread());
     }
 }
